dd4e6731174509d2dd9907b500663e24dd78a566
Carlos Lozano <carlos@adaptive.me>
2015-02-18 20:31:41 +0100

    API v2.1.7 Specification

--- adaptive-fx/adaptive-fx-lib/src/main/java/me/adaptive/arp/api/BaseCallbackImpl.java
+++ adaptive-fx/adaptive-fx-lib/src/main/java/me/adaptive/arp/api/BaseCallbackImpl.java
@@ -27,7 +27,7 @@ Contributors:
 
 Release:
 
-    * @version v2.1.6
+    * @version v2.1.7
 
 -------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
 */
@@ -83,7 +83,7 @@ public class BaseCallbackImpl implements IBaseCallback {
      */
      @Override
      public String getAPIVersion() {
-          return "v2.1.6";
+          return "v2.1.7";
      }
 
      /**
